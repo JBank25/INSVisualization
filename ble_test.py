443d768d31a3bd7429ced67effa07c303612e97c
JBank25 <urbankjoshua1@gmail.com>
2023-03-24 13:48:41 -0500

    Use this ble_test.py for GPS visualization

--- ble_test.py
+++ ble_test.py
@@ -12,10 +12,10 @@ BLE_UUID_GPS_VALUE = "beb5483e-36e1-4688-b7f5-ea07361b26a9"#"C8F88594-2217-0CA6-
 num_notifications = 0   #number of notifications received, debug purposes
 imu_data_queue = asyncio.Queue(-1)    #queue for storing imu data
 
-# # create a TCP/IP socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server_address = ('localhost', 10003)
-# sock.connect(server_address)
+# create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 10002)
+sock.connect(server_address)
 
 
 async def process_imu_data():
@@ -27,6 +27,7 @@ async def process_imu_data():
         imu_data = await imu_data_queue.get()
         #only send the last 3 entries in the list
         imu_data = json.dumps(imu_data[-3:])
+        
         print(f"Sending IMU data: {imu_data}")
         sock.sendall(imu_data.encode())
         # loop until we receive a '1' back from the socket
@@ -47,38 +48,36 @@ async def check_imu_data_queue():
             print(f"New IMU data available: {imu_data_queue.qsize()} items in queue.")
         await asyncio.sleep(1)  # sleep for 100ms before checking again
 
-def populate_data_map_imu(imu_data):
-    """
-    This function populates a dictionary with the IMU data
-    and returns the dictionary.
-    """
-    # Define the keys for the dictionary
-    keys = ["ordinalID", "imuData", "roll", "pitch", "yaw"]
-    data_dict = {}
-    data_dict[keys[0]] = imu_data[0]
-    data_dict[keys[1]] = imu_data[1:10]
-    data_dict[keys[2]] = imu_data[10]
-    data_dict[keys[3]] = imu_data[11]
-    data_dict[keys[4]] = imu_data[12]
-    return data_dict
-
 async def imu_handler(sender, data):
     """
     This function is called when a notification is received from the IMU.
     It will unpack the data and put it into the queue, rounding to 3 decimal places.
     """
-    imu_data = struct.unpack("<ffff", data)    #reading data from BLE, 13 floats
+    print(data)
+    imu_data = struct.unpack("<Bfff", data)    #reading data from BLE, 13 floats
     # imu_data = [round(x, 3) for x in imu_data]          #rounding to 3 decimal places
     print(f"Received IMU data: {imu_data}")
+    #wrirting data to file
+    with open("imu_data.txt", "a") as f:
+        f.write(f"imu: {', '.join(str(x) for x in imu_data)}\n")
     # await imu_data_queue.put(imu_data)                  #putting data into queue
     
 async def gps_handler(sender, data):
     global num_notifications
     num_notifications += 1
-    gps_data = struct.unpack("<ffff", data)
+    gps_packet = struct.unpack("<Bff", data)
     print(f"*************Received GPS data: {gps_data}**********************")
-    # with open("sensor_data.txt", "a") as f:
-    #     f.write(f"gps: {', '.join(str(x) for x in gps_data)}\n")
+    #only send the last 3 entries in the list
+    gps_data = json.dumps(gps_packet[-2:])
+    sock.sendall(gps_data.encode())
+    # loop until we receive a '1' back from the socket
+    while True:
+        response = sock.recv(1024)
+        if response == b'1':
+            print("Received '1' from socket")
+            break
+        else:
+            print(f"Received unexpected response from socket: {response}")
 
 
 async def main():
